40ee2251cc8555214d1d06ec72422b58381dae70
shogosakai <shogosakai73@gmail.com>
2020-08-06 12:37:11 +0900

    about type annotations.

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
-let hasValue: boolean = true;
-let count: number = 10;
-let float: number = 3.141;
-let negative: number = -123;
-let single: string = 'single';
-let double: string = "double";
-let back: string = `back`;
+let hasValue = true;
+let count = 10;
+let float = 3.141;
+let negative = -123;
+let single = 'single';
+let double = "double";
+let back = `back`;
+let hello;
+hello = 'hello';
+
+const person = {
+  name:{
+    first: 'Jack',
+    last: 'Smith'
+  },
+  age: 20
+}
+
+console.log(person);
